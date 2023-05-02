d038b3ca2437f4d4ef93419eb78bcf9c8b4389e4
DanielCarbajal <daniel.carbajal@pucp.edu.pe>
2023-05-02 13:31:24 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const html_to_pdf = require('html-pdf-node');
+const fs = require('fs')
+const options = { format: 'A4' };
+const file = { url: "http://127.0.0.1:5500/server/index.html" };
+html_to_pdf.generatePdf(file, options).then(pdfBuffer => {
+    fs.writeFileSync('./cv.pdf', pdfBuffer, 'binary');
+});
